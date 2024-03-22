7f598c2d8af2963a27d2ef285e83b60c2aa6e103
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-03-22 15:22:07 +0400

    Update findMin.java

--- findMin.java
+++ findMin.java
@@ -1,31 +1,27 @@
 import java.util.Scanner;
-public class findMin{
-  public static void main (String[] arg){
-    Scanner s=new Scanner(System.in);
+public class findMin {
+  public static void main(String[] arg) {
+    Scanner s = new Scanner(System.in);
     System.out.println("Enter the first number:");
-    double first=s.nextDouble();
+    double first = s.nextDouble();
     System.out.println("Enter the second number:");
-    double second=s.nextDouble();
+    double second = s.nextDouble();
     System.out.println("Enter the third number:");
-    double third=s.nextDouble();
-    System.out.println("The minimum value: "+minimum(first, second, third));
-    System.out.println("The maximum value: "+maximum(first, second, third));
+    double third = s.nextDouble();
+    System.out.println("The minimum value: " + minimum(first, second, third));
+    System.out.println("The maximum value: " + maximum(first, second, third));
     System.out.print("The average value: " + average(first, second, third));
-    
+
   }
-  public static double minimum(double first, double second, double third){
+  public static double minimum(double first, double second, double third) {
     return Math.min(Math.min(first, second), third);
-    }
-  public static double maximum(double first, double second, double third){
+  }
+  public static double maximum(double first, double second, double third) {
     return Math.max(Math.max(first, second), third);
   }
- 
-  
 
-  public static double average(double first, double second, double third){
-        return (first + second + third) / 3;
-    }
+  public static double average(double first, double second, double third) {
+    return (first + second + third) / 3;
+  }
 
 }
-
-    
